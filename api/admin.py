c5adc8cda4dd3b26068cae973f4812bc3b9a79ef
Karolina Kowasz <karolinamosur@gmail.com>
2019-03-23 09:09:50 +0100

    Admin for models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from api.models import Candidate, Recruiter, Task, Grade
 
-# Register your models here.
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name",)
+
+
+class RecruiterAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name",)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+
+
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ("candidate", "recruiter", "task", "value",)
+
+
+admin.site.register(Candidate, CandidateAdmin)
+admin.site.register(Recruiter, RecruiterAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Grade, GradeAdmin)
