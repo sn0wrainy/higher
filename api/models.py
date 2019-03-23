ad9e325947ce52e48883dcbca0b7d1b7ebaeaead
Karolina Kowasz <karolinamosur@gmail.com>
2019-03-23 08:45:01 +0100

    Models with initial migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Candidate(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+
+class Recruiter(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Grade(models.Model):
+    value = models.IntegerField()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    recruiter = models.ForeignKey(Recruiter, on_delete=models.CASCADE)
